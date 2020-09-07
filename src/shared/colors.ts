d587f0a127a5bc426dd1307eef55be9fa11f1daa
Arcyvilk <blazemakerrr@gmail.com>
2020-09-07 22:53:57 +0200

    :lipstick: global colors

--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,9 +1,13 @@
 const colors = {
-	background: '#364958',
-	primary: '#C9E4CA',
-	secondary: '#3B6064',
-	tertiary: '#55828B',
-	middle: '#55828B',
+	background: '#1B1725',
+	primary: '#D0BCD5',
+	secondary: '#A499B3',
+	tertiary: '#534B62',
+	middle: '#226CE0',
+
+	black: '#050505',
+	white: '#f0f0f0',
+	whiteish: '#F0DCF5',
 };
 
 export { colors };
